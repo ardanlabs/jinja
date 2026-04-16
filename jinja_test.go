787be2320f36ca673cfde52b6d12f5b8f92f992b
Miki Tebeka <miki@353solutions.com>
2026-04-16 19:54:36 +0300

    move regression to jinja_test

--- jinja_test.go
+++ jinja_test.go
@@ -907,6 +907,27 @@ func TestDictGet(t *testing.T) {
 	}
 }
 
+func TestRegression(t *testing.T) {
+	files, err := filepath.Glob("testdata/templates/*.jinja")
+	if err != nil {
+		t.Fatalf("can't read testdata/templates - %v", err)
+	}
+
+	for _, name := range files {
+		t.Run(name, func(t *testing.T) {
+			data, err := os.ReadFile(name)
+			if err != nil {
+				t.Fatalf("read - %v", err)
+			}
+
+			_, err = jinja.Compile(string(data))
+			if err != nil {
+				t.Fatalf("compile - %v", err)
+			}
+		})
+	}
+}
+
 var chatTemplates map[string]string
 
 func init() {
